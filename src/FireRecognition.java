8a12388259f8f308bad0a6a62d192ab72492d4af
Denisa Comsa <denisam700@gmail.com>
2022-02-12 22:07:06 +0200

    First commit: basic file recognition

---
+++ src/FireRecognition.java
@@ -0,0 +1,86 @@
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.highgui.HighGui;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.videoio.VideoCapture;
+
+public class FireRecognition {
+    public static void main(String[] args) {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+        Mat frame = new Mat();
+        Mat hsv = new Mat();
+        Mat blur = new Mat();
+        Mat mask = new Mat();
+        Mat output = new Mat();
+
+        VideoCapture fire_video = new VideoCapture("firevideo.mp4");
+        if (fire_video.isOpened()) {
+            while (true) {
+                fire_video.read(frame);
+                if (frame.empty()) break;
+
+                Imgproc.GaussianBlur(frame, blur, new Size(15, 15), 0);
+                Imgproc.cvtColor(frame, hsv, Imgproc.COLOR_BGR2HSV);
+
+                Scalar upper = new Scalar(18, 50, 50);
+                Scalar lower = new Scalar(35, 255, 255);
+                Core.inRange(hsv, upper, lower, mask);
+                Core.bitwise_and(frame, hsv, output, mask);
+
+                HighGui.imshow("Fire Recognition", frame);
+                HighGui.imshow("hsv", hsv);
+                HighGui.imshow("blur", blur);
+                HighGui.imshow("output", output);
+                int key = HighGui.waitKey(20);
+                if (key == 27)
+                    break;
+            }
+
+        }
+
+        HighGui.destroyAllWindows();
+        System.exit(0);
+    }
+}
+
+//        if (fire_video.isOpened()) {
+//            while (hasNext) {
+//                hasNext = fire_video.read(frame);
+//                numOfFrames++;
+//
+//                if (numOfFrames == 1000) {
+//                    break;  // calling this will break out of the loop
+//                }
+//
+//                HighGui.imshow("Fire Video", frame);
+//                int key = HighGui.waitKey(20);
+//                if (key == 27)
+//                    break;
+//            }
+
+//        JFrame jframe = new JFrame("Title");
+//        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        JLabel vidpanel = new JLabel();
+//        jframe.setContentPane(vidpanel);
+//        jframe.setVisible(true);
+//        while(hasNext) {
+//            hasNext = fire_video.read(frame);
+//            numOfFrames++;
+//
+//            if(numOfFrames == 100) {
+//                break;  // calling this will break out of the loop
+//            }
+//        } // end while loop
+
+//        while (true) {
+//            if (fire_video.read(frame)) {
+//
+//                ImageIcon image = new ImageIcon(new Mat2BufferedImage(frame));
+//                vidpanel.setIcon(image);
+//                vidpanel.repaint();
+//
+//            }
+//        }
